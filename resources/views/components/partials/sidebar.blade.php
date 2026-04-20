749c7f257c62204b8196609787e6e84a2b26152d
leviicd <pahlevimuslim123@gmail.com>
2026-04-20 14:55:44 +0700

    Tugas 4

--- resources/views/components/partials/sidebar.blade.php
+++ resources/views/components/partials/sidebar.blade.php
@@ -102,6 +102,12 @@
                 Dashboard Pasien
             </a>
 
+            <a href="{{ route('pasien.daftar') }}"
+                class="{{ $baseLink }} {{ request()->routeIs('pasien.daftar') ? $active : $inactive }}">
+                <i class="fas fa-house-medical w-4 text-center"></i>
+                Pendaftaran Periksa
+            </a>
+
 
         </div>
         @endif
@@ -120,6 +126,12 @@
                 class="{{ $baseLink }} {{ request()->routeIs('dokter.dashboard') ? $active : $inactive }}">
                 <i class="fas fa-stethoscope w-4 text-center"></i>
                 Dashboard Dokter
+            </a>
+
+            <a href="{{ route('jadwal-periksa.index') }}"
+                class="{{ $baseLink }} {{ request()->routeIs('jadwal-periksa.*') ? $active : $inactive }}">
+                <i class="fas fa-stethoscope w-4 text-center"></i>
+                Jadwal Periksa
             </a>
 
         </div>
